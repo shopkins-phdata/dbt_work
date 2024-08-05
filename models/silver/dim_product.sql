4b5c6511b92e31da86c037883fdfbb8d7927a1f3
Stacey <shopkins@phdata.io>
2024-08-05 15:16:51 -0500

    dim_product

---
+++ models/silver/dim_product.sql
@@ -0,0 +1,17 @@
+select 
+ md5(productid) as dim_product_key
+ , PRODUCTID
+ , PRODUCTNAME
+ , SUPPLIERID
+ , cat.categoryname
+ , QUANTITYPERUNIT
+ , UNITPRICE
+ , UNITSINSTOCK
+ , UNITSONORDER
+ , REORDERLEVEL
+ ,  case DISCONTINUED
+     when 1 then true
+     else false
+    end as is_discontinued
+from {{ ref('stg_products') }} prod
+ left join {{ ref('stg_categories') }} cat on prod.categoryid = cat.categoryid
